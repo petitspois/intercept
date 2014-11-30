f417f59a76f1594ed610f44d2bebbc7b457e42a0
damon <qingdoufd@163.com>
2014-11-30 20:15:39 +0800

    add tools and get forms

--- src/intercept.js
+++ src/intercept.js
@@ -1,35 +1,164 @@
-/*! bd.js 0.0.1 */
+/*! intercept.js 0.0.1 */
 
-(function (name, factory) {
-    if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
-        module.exports = factory();
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory;
     } else {
-        this[name] = factory;
+        root['$it'] = factory;
     }
-})('_bd', function (node, options) {
+})(this, function (options) {
+
+    "use strict";
+
+    var ruleArray = {};
+
+
+    var it = function (selector) {
+        var self = it.prototype,
+            elements = [];
+        if (typeof selector === 'string') {
+            elements = [].slice.call(document.querySelectorAll(selector));
+        } else if (typeof selector === 'object' && selector.attributes) {
+            elements = [selector];
+        }
+        self._elem = elements;
+        return self;
+    }
+
+    ////////////////////
+    // 为it添加工具方法 //
+    ////////////////////
+
+    it.prototype = {
+
+        version: '0.0.1',
+
+        constructor: it,
+
+        trim: function (str) {
+            return str.replace(/^\s+|\s+$/g, '');
+        },
+
+        isEmptyObject: function (obj) {
+            var name;
+            for (name in obj) {
+                return false;
+            }
+            return true;
+        },
+        mixIn: function (target) {
+            for (var i = 0, arg, args = arguments; arg = args[i++];) {
+                if (arg !== target) {
+                    for (var prop in arg) {
+                        target[prop] = arg[prop];
+                    }
+                }
+            }
+            return target;
+        }
+    }
+
+    ////////////////////
+    // 为it添加基础方法 //
+    ////////////////////
+    /* form--初始化 */
+    it.prototype.init = function () {
+        var formCollection = document.forms;
+        if (!formCollection.length)return;
+        loop : for (var i = 0, el; el = formCollection[i++];) {
+            var elAttr = el.getAttribute('it-controller');
+            if (typeof elAttr === 'string') {
+                if (!!$.trim(elAttr)) {
+                    ruleArray[elAttr] = el;
+                } else {
+                    ruleArray = el;
+                    break loop;
+                }
+            }
+        }
+        //执行清除it字段的其他原生属性
+        $.getClearAttr();
+    }
+
+    //清除原生属性，获取it属性
+    it.prototype.getClearAttr = function () {
+        if (!$.isEmptyObject(ruleArray)) {
+            if (ruleArray.nodeName) {
+                defaults.clearOriginal && $.clearOriginal(ruleArray);
+            } else {
+                alert(2)
+            }
+        }
+    }
+
+    //清除所有html5约束
+    it.prototype.clearOriginal = function (node) {
+        node = node.elements;
+
+        console.log(node)
+    }
+
+    it.prototype.findIt = function (node) {
+        console.log(node)
+    }
+
+    it.prototype.on = function (event, fn) {
+        var self = this,
+            elements = self._elem;
+        for (var i = 0, el; el = elements[i++];) {
+            if (document.addEventListener) {
+                el.addEventListener(event, fn, false);
+            } else if (document.attachEvent) {
+                el.attachEvent('on' + event, fn);
+            }
+        }
+    }
+
+    it.prototype.ready = function (callback) {
+        var fired = false;
+
+        function trigger() {
+            if (fired) return;
+            fired = true;
+            callback();
+        }
+
+        if (document.readyState === 'complete') {
+            setTimeout(trigger);
+        } else if (document.addEventListener) {
+            document.addEventListener('DOMContentLoaded', trigger, false);
+            window.addEventListener('load', trigger, false);
+        } else {
+            document.attachEvent('onreadystatechange', function () {
+                if (document.readyState === 'complete') {
+                    trigger();
+                }
+            });
+        }
+    }
+
+    //////////////
+    //   实例化  //
+    //////////////
+
+    var $ = new it();
 
     var defaults = {
-        username : '|',
-    };
+        'clearOriginal': true
+    }
 
-    var nodes = $(node).find('input[bd-rule]'),
-        nodesArr = [];
+    //合并基础配置
+    $.mixIn(defaults, options.base);
 
-    $.each(nodes,function(k,v){
-        $(this).on('blur',function(){
-            console.log($(this).attr('bd-rule'))
-        });
+    $.ready(function () {
+        $.init();
+
     });
 
-    console.log(nodesArr)
-
-
-
-
-//    $.bd = function(options){
-//        console.log(this[0].nodeName)
-//    }
-
+    //返回公共对象
+    return {'version': $.version, 'url': 'qingdou.me'}
 
 });
 
