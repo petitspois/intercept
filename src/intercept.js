a3b55cb3eebce237995497344866fd1036d64d9b
damon <qingdoufd@163.com>
2014-12-18 17:53:35 +0800

    添加信息详情

--- src/intercept.js
+++ src/intercept.js
@@ -20,6 +20,10 @@
 
     var i8 = !!('\v' == 'v');
 
+    var hook = '该字段验证通过!';
+
+    var isRequiredSupported = "required" in document.createElement("input");
+
     //forms集合
     var ruleArray = {};
 
@@ -81,16 +85,26 @@
             return [].slice.call(iterable);
         },
         getItAttr: function (attrs) {
-            var itA = [], i = 0, attr,
+            var itA = [],
+                i = 0,
+                attr,
+                nodeName,
+                must = false,
                 chaos = $.toArray(attrs);
             while (attr = chaos[i++]) {
-                var nodeName = attr.nodeName;
-                if (nodeName !== 'it-messages') {
-                    (nodeName.charAt(2) === '-' && nodeName.slice(0, 2) === 'it') && itA.push(attr);
-                } else {
-                    itA.unshift(attr);
+                nodeName = attr.nodeName;
+                switch (nodeName) {
+                    case 'it-messages':
+                        itA.unshift(attr);
+                        break;
+                    case 'required':
+                        must = true;
+                        break;
+                    default :
+                        (nodeName.charAt(2) === '-' && nodeName.slice(0, 2) === 'it') && itA.push(attr);
                 }
             }
+            must && (itA.splice(1, 0, 'required'));
             return itA;
         },
         camelize: function (target) {
@@ -109,6 +123,46 @@
         },
         isAll: function () {
             return !!defaults['all'];
+        },
+        novalidate : function(control){
+            isRequiredSupported && (ruleArray[control]['noValidate'] = true);
+        },
+        /**
+         *
+         * 获取信息内容
+         * @param {number} type 0=>成功,1=>失败,2=>提示
+         * @param {string} mess 提示信息
+         * @param {string} ctrl 控制范围
+         * @param {object} el 当前元素
+         * @param {string} frNe 过滤属性
+         */
+        getMess: function (mess, type, ctrl, el, frNe) {
+            var elName = el.name,
+                messContent = defaults[ctrl]['messContent'][elName];
+            if (!!mess) {  //dom上有it-messages的情况
+                var messColl = mess.split('|');
+                    return this.returnMess(messColl, type, ctrl, elName, frNe);
+            } else {
+                //$it({}) 配置项 default=>ctrl=>'messContent'=>currentName
+                if(messContent){
+                    return this.returnMess(messContent, type, ctrl, elName, frNe);
+
+                //启用默认
+                }else{
+
+                    return this.returnMess(defaults[ctrl]['defaultMess'][frNe], type);
+                }
+            }
+
+        },
+        returnMess:function(messColl, type, ctrl, elName, frNe){
+            if(messColl[type] === undefined){
+                if(defaults[ctrl]['messContent'][elName][type]!==undefined){
+                     return defaults[ctrl]['messContent'][elName][type];
+                }
+                return defaults[ctrl]['defaultMess'][frNe][type];
+            };
+            return messColl[type];
         }
     }
 
@@ -214,11 +268,22 @@
             var itArray = $.getItAttr(elem.attributes),
                 itLen = itArray.length, i = 0, ii, node, mg;
             while (node = itArray[i++]) {
-                var filterName = $.camelize(node.nodeName),
+                /**
+                 *  @var 'filtername' {intercept 属性集合}
+                 *       'mess' {dom中是否有it-messages属性}
+                 *       'isVia' {filter验证是否通过}
+                 *       'control' {控制范围}
+                 */
+                var filterName = $.camelize(node.nodeName || node),
                     mess = !!$.Observer[filterName],
-                    isVia = $scope.$filter[filterName] && $scope.$filter[filterName](newValue, node.value),
+                    isVia,
                     control = controller;
-                if (isVia || (mess && (mg = node.value))) {
+                //集合第一条不为it-messages
+                !mess && (isVia = $scope.$filter[filterName] && $scope.$filter[filterName](newValue, (node.value || node)));
+                //集合第一条为it-messages,对mg进行赋值
+                mess && (mg = node.value);
+                //验证返回值或信息为true时
+                if (isVia || mess) {
                     void function () {
                         ii = i
                     }();
@@ -228,14 +293,25 @@
                     //插入验证成功数据
                     flawless[control][name] = newValue;
                     //执行媒体操作
-                    $.Observer.itMessages.success(mg, elem, control);
+                    $.Observer.itMessages.success(mg, elem, control, filterName);
                 } else {
-                    $.Observer.itMessages.error(mg, elem, control);
+                    $.Observer.itMessages.error(mg, elem, control, filterName);
                     break;
                 }
             }
             //执行过滤行为
         });
+    }
+    //提交处理
+    it.prototype.submit = function () {
+        for(var prop in controller){
+            //阻止高级浏览器验证
+            $.novalidate(prop);
+            it(ruleArray[prop]).on('submit',function(e){
+
+                e.preventDefault();
+            });
+        }
     }
 
     it.prototype.on = function (event, fn) {
@@ -283,26 +359,43 @@
 
     it.prototype.Observer = {
         itMessages: {
-            success: function (mess, el) {
-                mess = mess.split('|')[0];
-                this.trusteeship(mess, el);
+            success: function (mess, el, ctrl, frNe) {
+                mess = $.getMess(mess, 0, ctrl, el, frNe);
+                this.trusteeship(1, mess, el);
             },
-            error: function (mess, el) {
-                mess = mess.split('|')[1];
-                this.trusteeship(mess, el);
+            error: function (mess, el, ctrl, frNe) {
+                mess = $.getMess(mess, 1, ctrl, el, frNe);
+                this.trusteeship(0, mess, el);
             },
-            warning: function () {
+            info: function () {
 
             },
-            trusteeship: function (content, el) {
+            /**
+             *
+             * 信息处理托管
+             * @param {number} type 1=>成功,0=>失败,2=>提示
+             * @param content 提示信息
+             * @param {object} 操作元素
+             *
+             */
+            trusteeship: function (type, content, el) {
                 var control = el.form.getAttribute('it-controller'),
-                    option = $.isAll() ? defaults['all'] : defaults[control],
-                    node = option['messDepth'] === 'sibling' ? it(el).siblings() : false;
+                    option = $.isAll() ? defaults['all'] : defaults[control], node,
+                    sibling = it(el).siblings();
+                if (sibling === null) throw new SyntaxError('请添加相邻元素!');
+                node = option['messDepth'] === 'sibling' ? sibling : false;
                 if (node) {
+                    type ? node.className = 'text-success' : node.className = 'text-error';
                     node.innerHTML = content;
                 } else {
                     if (option['messDepth'] === 'children') {
-                        it(el).siblings().children[0].innerHTML = content
+                        var child = sibling.children[0];
+                        if (child) {
+                            type ? child.className = 'text-success' : child.className = 'text-error';
+                            child.innerHTML = content;
+                        } else {
+                            throw new SyntaxError('请在信息中添加子元素!');
+                        }
                     } else {
                         throw new SyntaxError('请正确填写"messDepth"值!');
                     }
@@ -346,9 +439,13 @@
     }
 
     Scope.prototype.$filter = {
+        //arguments (value,rule)
         'itMaxlength': function () {
             var args = $.toArray(arguments);
             return args[0].length <= (+args[1]);
+        },
+        'required': function (value) {
+            return $.trim(value) !== '';
         }
     }
 
@@ -362,18 +459,29 @@
     var $scope = new Scope();
 
     var originDefaults = {
+            //插入信息深度
             'messDepth': 'children',
-            'tap': 'click'
+            //默认过滤信息
+            'defaultMess' :{
+                itMaxlength:{
+                    0:hook,
+                    1:'超过字符最大长度!'
+                },
+                required:{
+                    0:hook,
+                    1:'该字段不能为空!'
+                }
+            }
         },
         defaults = {};
 
     //合并基础配置
-    if(typeof options==='object' && !$.isEmptyObject(options)) {
+    if (typeof options === 'object' && !$.isEmptyObject(options)) {
         for (var prop in options) {
             defaults[prop] = $.mixIn({}, originDefaults);
             $.mixIn(defaults[prop], options[prop]);
         }
-    }else{
+    } else {
         defaults.all = originDefaults;
     }
     //domReady自动运行
@@ -382,7 +490,8 @@
         $.init();
         //绑定事件处理
         $.events();
-
+        //提交操作
+        $.submit();
     });
 
     //返回公共对象
